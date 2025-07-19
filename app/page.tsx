a9a7ad531248adb31b5f4dd92615f2991e0aebb7
darwin403 <skdcodes@gmail.com>
2025-07-19 19:19:50 +0530

    fix: nuqs useQueryState error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect, useRef, memo, type ReactNode } from "react";
 import { useQueryState } from "nuqs";
 import { Input } from "../components/ui/input";
